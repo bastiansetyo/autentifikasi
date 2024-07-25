9075f4e368f7d3f37a6548c72e22655ce7d64fd2
NairB <brian.koeswanto@infovesta.com>
2024-07-25 15:13:13 +0700

    first commit

---
+++ src/test/java/com/project/autentifikasi/AutentifikasiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.autentifikasi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutentifikasiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
